43affbb7259bad39b72de4b22bc3d10ba465c037
Ryan Thomas <rythomas2525@gmail.com>
2020-05-20 14:35:43 -0600

    address bar edited

--- src/components/Addressjumbotron/index.js
+++ src/components/Addressjumbotron/index.js
@@ -12,20 +12,27 @@ function Addressjumbotron() {
 
         <div className=" addressjumbo" style={{ width: "100vw" }}>
             <Container >
+                <Row>
+                    <Col size="md-4"></Col>
+                    <Col size="md-4">
+                        <h1 className="cafeaddress text-center mt-4">Eagle Café</h1>
+                    </Col>
+                    <Col size="md-4"></Col>
+                </Row>
                 <Row >
-                    <Col size="md-3" className="address ml-3 mr-2 text-center"  >
+                    <Col size="md-4" className="address ml-4 mr-2 text-center"  >
                         <h1 >Address</h1>
                         <h2>6345 Murray Pkwy Ave</h2>
                         <h2> Murray, UT 84123</h2>
                     </Col>
 
-                    <Col size="md-3" className="address ml-1 mr-2 text-center"  >
+                    <Col size="md-4" className="address ml-1 mr-2 text-center"  >
                         <h1 >Address</h1>
                         <h2>6345 Murray Pkwy Ave</h2>
                         <h2> Murray, UT 84123</h2>
                     </Col>
 
-                    <Col size="md-3" className="addressnoline ml-1 mr-2 text-center"  >
+                    <Col size="md-4" className="addressnoline ml-1  text-center"  >
                         <h1 >Address</h1>
                         <h2>6345 Murray Pkwy Ave</h2>
                         <h2> Murray, UT 84123</h2>
