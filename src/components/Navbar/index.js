f86c99b6bfdb12b3f1321099cc050fe95ae7e8b3
Ryan Thomas <rythomas2525@gmail.com>
2020-05-19 22:11:46 -0600

    cleaned up css code

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,10 +10,9 @@ function Navbar() {
     return (
 
         <nav className="navbar navbar-expand-xl navbar-light sticky-nav">
-
             <Link className="navbar-brand eaglecafe navhover justify-content-center  " to="/">
                 Eagle Café
-      </Link>
+                </Link>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
