2a3a75bc74971dac60d5207a849d5f8da3f83003
Ryan Thomas <rythomas2525@gmail.com>
2020-05-26 22:14:02 -0600

    fixed flag on banners

--- src/components/Itemcard/index.js
+++ src/components/Itemcard/index.js
@@ -4,8 +4,8 @@ import Row from '../Row';
 import Col from '../Col';
 import Container from '../Container';
 import Instagram from '../Instagram';
-
-
+import Baconcheese from '../Baconcheese'
+import baconcheese from "./baconcheese.jpg"
 
 
 function Itemcard() {
@@ -13,7 +13,7 @@ function Itemcard() {
 
         <div className="itemcard text-center justify-content-center">
             <hr></hr>
-
+            <img className="rounded-circle bg-white mt-4 baconcheeseround" alt="Bacon Cheeseburger" src={baconcheese} />
             <h1>Hello World</h1>
 
         </div>
